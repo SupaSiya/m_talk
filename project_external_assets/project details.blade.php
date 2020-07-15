38237e783f445ca4e1d302088604de5d911f8618
Wordsworth Luvatsha <supaseer@gmail.com>
2020-07-15 20:11:23 +0200

    initial commit

---
+++ project_external_assets/project details.blade.php
@@ -0,0 +1,54 @@
+/**
+    Project color scheme.
+*/
+Current Logo colours
+
+L = Green #008037
+Aqua blue #00c2cb
+M = Dark grey #545454
+A = Grass green #7ed957
+S = #8d2a68
+Light grey #a8a8a8
+K = #d47e25
+Orange #ff914d
+
+/**
+    Project information, sections data.
+*/
+[10:58, 7/15/2020] Marvelous: What we do 
+MTalks provides hands-on digital and data services to companies and organisations
+which enriches their service offering and enables them to effectively manage large
+datasets. In doing so, MTalks is able to provide dignified, steady, fair-wage work to
+unemployed and previously disadvantaged youth.
+
+The Problem
+MTalks is addressing a two-fold problem. One being that suppliers of services and products particularly in the tech space have a challenge when it comes to the last mile delivery of their products and sustained use of services in underdeveloped areas, such as townships and rural communities. The other problem is the nationwide problem of youth unemployment; and with COVID-19 both these problems have only matured in state. 
+
+
+How MTalks Addresses The Problem
+MTalks provides a project-based learning approach that uses technology services in
+order to create employment for youth, while building digital skills. Our project
+handling is done by minimizing projects into smaller scales of work or tasks in order to
+redistribute to youth, this is accompanied by the training required in order complete
+the task, where in turn, the youth get to make money on work completed.
+
+MTalks uses an internal interactive web-based work platform which enables us to
+break projects up into smaller tasks and link them up with the necessary content or
+study for the skill required to complete the task. Through the platform, we are able to
+enroll and onboard youth from various rural communities and leverage them as
+service providers to surrounding businesses. Through this, we are able to set up virtual hubs where each hub member creates a profile that allows them to build a track record as they take on more tasks, linked to key performance indicators. The platform will also be used for data collation, tracking of project tasks, and reporting. This platform is a training platform that is used to manage the entire lifecycle of a project, despite COVID-19 setbacks. The platform ensures high-quality training and validation for quality service delivery to clients.
+
+Our hubs members are skilled young people who are from these communities and are able to distribute and manage products and services to members within these communities as trusted persons of those communities, giving companies access into the untapped markets of rural and township communities, and a wide scale of BBBEE scoring.
+[10:59, 7/15/2020] Marvelous: a quick cut an paste from some of our content, let me know if you have any questions
+
+
+/**
+    tabs/ sections
+*/
+My tabs will be 
+Home
+About 
+How it work 
+BBBEE befenits 
+Contact
+
