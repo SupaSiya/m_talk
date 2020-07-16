8bf49dcdc0bc302dcbbaead1eeead98b67c0a561
Wordsworth Luvatsha <supaseer@gmail.com>
2020-07-16 12:13:30 +0200

    fully refactored and contacts work.. excpt bbbee

--- resources/views/components/landing-main-section.blade.php
+++ resources/views/components/landing-main-section.blade.php
@@ -292,36 +292,68 @@
 
                 <div class="col-lg-8 mt-5 mt-lg-0">
 
-                    <form action="{{ route('contact-us') }}" method="post" role="form" class="php-email-form">
+                    <form action="{{ route('contact-us') }}" method="post" role="form">
                         @csrf
                         <div class="form-row">
+                            
                             <div class="col-md-6 form-group">
-                            <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                            <div class="validate"></div>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" placeholder="Full Name" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
+
                             <div class="col-md-6 form-group">
-                            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                            <div class="validate"></div>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="E-mail" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                            <div class="validate"></div>
+                            <input id="subject" type="text" class="form-control @error('subject') is-invalid @enderror" name="subject" value="{{ old('subject') }}" placeholder="Subject" required autocomplete="subject">
+
+                                @error('subject')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                         </div>
 
                         <div class="form-group">
-                            <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                            <div class="validate"></div>
+                            <textarea id="message" class="form-control @error('message') is-invalid @enderror" name="message" value="{{ old('message') }}" placeholder="type message here..." required autocomplete="message"></textarea>
+
+                                @error('message')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                         </div>
-                        <div class="mb-3">
+
+                        {{--<div class="mb-3">
                             <div class="loading">Loading</div>
-                            <div class="error-message"></div>
+                             <div class="error-message"></div> 
                             <div class="sent-message">Your message has been sent. Thank you!</div>
-                        </div>
-                        <div class="text-center"><button type="submit">Send Message</button></div>
-                    </form>
+                        </div>--}}
 
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Send Message') }}
+                            </button>
+                        </div>
+                    </form>
+                    <div class="feedback">
+                        @if(Session::has('message'))
+                            <p class="alert alert-success">{{ Session::get('message') }}</p>
+                        @endif
+                    </div>
                 </div>
 
             </div>
